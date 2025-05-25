3a8a59b391855d26509a1b4ae65213299680de75
ADMIN <l18201107096@163.com>
2025-05-25 10:14:38 +0800

    init

---
+++ main.js
@@ -0,0 +1,34 @@
+const { app, BrowserWindow } = require('electron/main')
+
+// 创建一个窗口
+const creatWindow = () => {
+  const win = new BrowserWindow({
+    width: 800, // 应用页面宽
+    height: 800, // 应用页面高
+    // autoHideMenuBar: true, // 是否隐藏默认菜单栏
+    // x: 0, // 初始化水平位置
+    // y: 0, // 初始化垂直位置
+    // alwaysOnTop: true // 是否一直保持最上面
+  })
+  // 加载页面 哔哩哔哩页面
+  // win.loadURL('https://www.bilibili.com/')
+  // 加载本地文件
+  win.loadFile('index.html')
+}
+
+app.on('ready', () => {
+  // 执行创建窗口函数
+  creatWindow();
+  console.log('--')
+  app.on('activate', () => {
+    // 苹果macos 系统没有窗口创建一个窗口
+    if (BrowserWindow.getAllWindows().length === 0) {
+      creatWindow();
+    }
+  })
+})
+
+app.on('window-all-closed', () => {
+  // 判断是不是苹果系统 如果不是 退出应用
+  if (process.platform != 'darwin') app.quit();
+})
